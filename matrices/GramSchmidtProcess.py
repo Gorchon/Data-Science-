344ad6b401673c486a8094dee4f4cfc1b2bfa1d3
Gorchon <a01254831@tec.mx>
2024-04-30 14:20:20 -0600

    GRAM IN MATRICES

--- matrices/GramSchmidtProcess.py
+++ matrices/GramSchmidtProcess.py
@@ -10,35 +10,37 @@ verySmallNumber = 1e-14 # That's 1×10⁻¹⁴ = 0.00000000000001
 # to all the vectors that came before it. Before normalising.
 # Follow the instructions inside the function at each comment.
 # You will be told where to add code to complete the function.
-def gsBasis4(A) :
-    B = np.array(A, dtype=np.float_) # Make B as a copy of A, since we're going to alter it's values.
-    # The zeroth column is easy, since it has no other vectors to make it normal to.
-    # All that needs to be done is to normalise it. I.e. divide by its modulus, or norm.
-    B[:, 0] = B[:, 0] / la.norm(B[:, 0])
-    # For the first column, we need to subtract any overlap with our new zeroth vector.
-    B[:, 1] = B[:, 1] - B[:, 1] @ B[:, 0] * B[:, 0]
-    # If there's anything left after that subtraction, then B[:, 1] is linearly independant of B[:, 0]
-    # If this is the case, we can normalise it. Otherwise we'll set that vector to zero.
-    if la.norm(B[:, 1]) > verySmallNumber :
-        B[:, 1] = B[:, 1] / la.norm(B[:, 1])
-        
-        else:
-        B[:, 1] = np.zeros_like(B[:, 1])
+def gsBasis4(A):
+    B = np.array(A, dtype=np.float_)  # Copy input matrix A to B to avoid modifying A directly.
+    
+    # Normalize the zeroth column (first basis vector)
+    B[:, 0] = B[:, 0] / la.norm(B[:, 0])  # Divide by its norm to make it a unit vector.
 
-    # Orthogonalize second column
+    # Process the first column
+    # Remove projection of the first column onto the normalized zeroth column
+    B[:, 1] = B[:, 1] - B[:, 1] @ B[:, 0] * B[:, 0]
+    # Normalize the first column if it is not near zero
+    if la.norm(B[:, 1]) > verySmallNumber:
+        B[:, 1] = B[:, 1] / la.norm(B[:, 1])
+    else:
+        B[:, 1] = np.zeros_like(B[:, 1])  # Set to zero if it's dependent on the zeroth column
+
+    # Process the second column
+    # Remove projection onto the zeroth and first column
     B[:, 2] = B[:, 2] - B[:, 2] @ B[:, 0] * B[:, 0]
     B[:, 2] = B[:, 2] - B[:, 2] @ B[:, 1] * B[:, 1]
-    # Normalization of the second column
+    # Normalize the second column
     if la.norm(B[:, 2]) > verySmallNumber:
         B[:, 2] = B[:, 2] / la.norm(B[:, 2])
     else:
         B[:, 2] = np.zeros_like(B[:, 2])
 
-    # Orthogonalize third column
+    # Process the third column
+    # Remove projection onto all previous columns
     B[:, 3] = B[:, 3] - B[:, 3] @ B[:, 0] * B[:, 0]
     B[:, 3] = B[:, 3] - B[:, 3] @ B[:, 1] * B[:, 1]
     B[:, 3] = B[:, 3] - B[:, 3] @ B[:, 2] * B[:, 2]
-    # Normalization of the third column
+    # Normalize the third column
     if la.norm(B[:, 3]) > verySmallNumber:
         B[:, 3] = B[:, 3] / la.norm(B[:, 3])
     else:
@@ -46,55 +48,25 @@ def gsBasis4(A) :
 
     return B
 
-    
-    # Finally, we return the result:
-    return B
-    #else :
-     #   B[:, 1] = np.zeros_like(B[:, 1])
-    # Now we need to repeat the process for column 2.
-    # Insert two lines of code, the first to subtract the overlap with the zeroth vector,
-    # and the second to subtract the overlap with the first.
-    
-    
-    # Again we'll need to normalise our new vector.
-    # Copy and adapt the normalisation fragment from above to column 2.
-    
-    
-    
-    
-    # Finally, column three:
-    # Insert code to subtract the overlap with the first three vectors.
-    
-    
-    
-    # Now normalise if possible
-    
-    
-    
 
-    
-    # Finally, we return the result:
-    return B
 
 # The second part of this exercise will generalise the procedure.
 # Previously, we could only have four vectors, and there was a lot of repeating in the code.
 # We'll use a for-loop here to iterate the process for each vector.
-def gsBasis(A) :
-    B = np.array(A, dtype=np.float_) # Make B as a copy of A, since we're going to alter it's values.
-    # Loop over all vectors, starting with zero, label them with i
-    for i in range(B.shape[1]) :
-        # Inside that loop, loop over all previous vectors, j, to subtract.
-        for j in range(i) :
-            # Complete the code to subtract the overlap with previous vectors.
-            # you'll need the current vector B[:, i] and a previous vector B[:, j]
-            B[:, i] = 
-        # Next insert code to do the normalisation test for B[:, i]
-        if :
-            
-        
-            
-    # Finally, we return the result:
+
+def gsBasis(A):
+    B = np.array(A, dtype=np.float_)  # Create a mutable copy of A as B.
+    for i in range(B.shape[1]):  # Loop over each column vector in matrix B.
+        for j in range(i):  # Loop over all previous vectors to orthogonalize against.
+            # Subtract the component of the i-th vector that is in the direction of the j-th vector.
+            B[:, i] = B[:, i] - B[:, i] @ B[:, j] * B[:, j]
+        # After orthogonalization, check if the vector is non-zero and normalize it.
+        if la.norm(B[:, i]) > verySmallNumber:
+            B[:, i] = B[:, i] / la.norm(B[:, i])
+        else:
+            B[:, i] = np.zeros_like(B[:, i])  # If the vector is near zero, set it to zero vector.
     return B
+
 
 # This function uses the Gram-schmidt process to calculate the dimension
 # spanned by a list of vectors.
